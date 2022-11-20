fe0af88ae4915b261039a6d217f4835f5c7f550e
RenanCecchin <renan.cecchin@hotmail.com>
2022-11-20 20:21:32 -0300

    Retirada do State.py, e adicionado suporte a entrada padrão

--- State.py
+++
@@ -1,6 +0,0 @@
-class State:
-    def __init__(self, name):
-        self.name = name    #Nome do estado
-
-    def getID(self):
-        return self.name
--- main.py
+++ main.py
@@ -1,42 +1,41 @@
+#Grupo:
+#Andriza, Augusto e Renan
+
 import sys
 from Transition import Transition
-from State import State
 from copy import copy
 from ReversibleTuringMachine import ReversibleTuringMachine
 
-if(sys.argv[1] != None):
-    content = open(sys.argv[1], 'r').readlines()
-    num_states, num_input_symbols, num_queue_symbols, num_transitions = content[0].split()
-    str_states = content[1].split()
-    states = []
+file = sys.stdin
 
-    for state in str_states:
-        states.append(State(state))
+content = file.readlines()
+num_states, num_input_symbols, num_queue_symbols, num_transitions = content[0].split()
+str_states = content[1].split()
 
-    input_symbols = content[2].split()
-    queue_symbols = content[3].split()
-    transitions = []
+input_symbols = content[2].split()
+queue_symbols = content[3].split()
+transitions = []
 
-    for i in range(4, 4 + int(num_transitions)):
-        transitions.append(Transition(content[i]))
-        
-    reversibleTuringMachine = ReversibleTuringMachine(num_states, input_symbols, queue_symbols, transitions)
+for i in range(4, 4 + int(num_transitions)):
+    transitions.append(Transition(content[i]))
+    
+reversibleTuringMachine = ReversibleTuringMachine(num_states, input_symbols, queue_symbols, transitions)
 
-    state_marker = 1
-    head_marker = 0
-    entry = content[-1]
-    input_tape = entry + "B"
-    input_tape = list(input_tape)
-    history_tape = []
+state_marker = 1
+head_marker = 0
+entry = content[-1]
+input_tape = entry + "B"
+input_tape = list(input_tape)
+history_tape = []
 
-    reversibleTuringMachine.makeTransitions(state_marker, head_marker, input_tape, history_tape)
-    output_tape = copy(input_tape)
+reversibleTuringMachine.makeTransitions(state_marker, head_marker, input_tape, history_tape)
+output_tape = copy(input_tape)
 
-    print("FITA DE INPUT(antes de reverter):", input_tape)
-    print("FITA DE HISTORICO(para revercao)", history_tape)
+print("FITA DE INPUT(antes de reverter):", input_tape)
+print("FITA DE HISTORICO(para reversao)", history_tape)
 
-    reversibleTuringMachine.reverse_movement(history_tape, head_marker, input_tape)
+reversibleTuringMachine.reverse_movement(history_tape, head_marker, input_tape)
 
-    print("FITA DE INPUT REVERTIDA:", input_tape)
-    print("FITA DE OUTPUT", output_tape)
-    print("FITA DE HISTORICO FINAL", history_tape)
+print("FITA DE INPUT REVERTIDA:", input_tape)
+print("FITA DE HISTORICO FINAL", history_tape)
+print("FITA DE OUTPUT", output_tape)
